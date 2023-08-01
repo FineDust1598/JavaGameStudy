ec96988de6d1dc161c4325db28ed3f9910da8c36
FineDust1598 <us2922142@gmail.com>
2023-08-01 14:54:31 +0900

    클래스를 메인에서 빼내어 다시 제작 게임 화면 구성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.LocalDateTime;
+import java.util.Random;
 public class Main {
     //정수인지 확인하는 메서드
     static boolean isInteger(String strValue){
@@ -27,29 +29,48 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        //메인화면 만들기
         String a = "";
         int checkNum;
+        String[] wheatName = {"쌀", "보리", "옥수수", "현미"};
+        String[] vegeName = {"당근", "감자", "고구마", "호박", "오이"};
+        String[] fruitName = {"사과", "오렌지", "메론", "레몬", "두리안", "바나나"};
+
+        Random rand = new Random();
+        wheat[] wheatList = new wheat[10];
+        vege[] vegeList = new vege[10];
+        fruit[] fruitList = new fruit[10];
+        for (int i = 0; i < wheatList.length; i++) {
+            wheatList[i] = new wheat(0, rand.nextInt(91)+10, wheatName[i%4], "wheat");
+        }
+        for (int i = 0; i < vegeList.length; i++) {
+            vegeList[i] = new vege(0, rand.nextInt(91)+10, vegeName[i%5], "wheat");
+        }
+        for (int i = 0; i < fruitList.length; i++) {
+            fruitList[i] = new fruit(0, rand.nextInt(91)+10, fruitName[i%6], "wheat");
+        }
+
+        //메인화면 만들기
         //시작화면 출력
         Screen sr = new Screen();
         sr.startScreen();
         //입력값 받기
         Scanner sc = new Scanner(System.in);
         a = sc.next();
-        fruit Fruit = new fruit(0,100,"두리안", "Fruit");
-        vege Vege = new vege(0, 100, "당근", "Vege");
-        wheat Wheat = new wheat(0, 100, "쌀", "wheat");
 
         while (true){
             checkNum = inputCheck(a);
             if(checkNum==1){
                 System.out.println("게임 플레이");
                 sr.startingScreen();
-                sr.gameScreen("Null", Fruit.name, Fruit.type, "■ ■ ■");
+                sr.gameScreen("Null", "Null", "Null", "■ ■ ■");
                 break;
             }
             else if(checkNum==2){
                 System.out.println("세팅화면 출력");
+                for(int i = 0; i< wheatList.length;i++){
+                    System.out.println(wheatList[i].health);
+                    System.out.println(wheatList[i].name);
+                }
                 break;
             }
             else if(checkNum==3){
